e1891a068cebb3bf511612cf92b6943cda9f95d9
Satoshi T <sulp91_fy@yahoo.co.jp>
2021-09-13 02:00:10 +0900

    Fix message

--- index.js
+++ index.js
@@ -2,38 +2,29 @@ const {
   CostExplorerClient,
   GetCostAndUsageCommand,
 } = require("@aws-sdk/client-cost-explorer");
-const line = require('@line/bot-sdk');
+const line = require("@line/bot-sdk");
 
-const client = new line.Client({channelAccessToken: process.env.ACCESS_TOKEN});
+const client = new line.Client({
+  channelAccessToken: process.env.ACCESS_TOKEN,
+});
 const REGION = "ap-northeast-1";
 
 // async/await.
-const getMonthlyAmount = async (in_ymd_from) => {
+const getMonthlyAmount = async (target) => {
   try {
     const client = new CostExplorerClient({ region: REGION });
-    const BASE = new Date(2021, 8, 1);
-    //const BASE = new Date();
+    const BASE = new Date(target.getFullYear(), target.getMonth(), 1);
     const NEXT_MONTH = new Date(BASE.getFullYear(), BASE.getMonth(), 1);
     NEXT_MONTH.setMonth(NEXT_MONTH.getMonth() + 1);
 
-    const day_from = new Date(BASE.getFullYear(), BASE.getMonth(), 1, 9, 0, 0);
-    const day_to = new Date(
-      NEXT_MONTH.getFullYear(),
-      NEXT_MONTH.getMonth(),
-      1,
-      9,
-      0,
-      0
-    );
-
-    const ymd_from = day_from.toISOString().substring(0, 10);
-    const ymd_to = day_to.toISOString().substring(0, 10);
+    const day_from = new Date(BASE.getFullYear(), BASE.getMonth(), 1);
+    const day_to = new Date(NEXT_MONTH.getFullYear(), NEXT_MONTH.getMonth(), 1);
 
     // memo: Start->include, End->not inculude
     const params = {
       TimePeriod: {
-        Start: ymd_from,
-        End: ymd_to,
+        Start: toYMDString(day_from),
+        End: toYMDString(day_to),
       },
       Granularity: "MONTHLY",
       Metrics: ["UnblendedCost"],
@@ -47,31 +38,45 @@ const getMonthlyAmount = async (in_ymd_from) => {
   } catch (error) {
     // error handling.
     console.log(error);
-    return '';
+    return "";
   }
 };
 
 const parseFloatAndFloor = (str_val) => {
   return Math.floor(parseFloat(str_val) * 1000) / 1000;
-}
+};
+
+const toYMDString = (dt) => {
+  const mm = ('00' + (dt.getMonth()+1)).slice(-2);
+  const dd = ('00' + dt.getDate()).slice(-2);
+  return `${dt.getFullYear()}-${mm}-${dd}`;
+};
 
 exports.handler = async (event) => {
-    console.log('event:', event);
-    const event_data = JSON.parse(event.body);
-    console.log('event.body:', JSON.stringify(event_data));
-    const messageData = event_data.events && event_data.events[0];
-    console.log('text:' + JSON.stringify(messageData.message.text));
+  console.log("event:", event);
+  const event_data = JSON.parse(event.body);
+  console.log("event.body:", JSON.stringify(event_data));
+  const messageData = event_data.events && event_data.events[0];
+  console.log("text:" + JSON.stringify(messageData.message.text));
 
-    const amount_str = await getMonthlyAmount('202109'); //TODO 日付指定はまだ
-    const message = amount_str ? `期間: XXXXXXXX～XXXXXXX\nAWS利用料: ${parseFloatAndFloor(amount_str)}$` : 'AWS料金の取得に失敗しました😩';
-    const postData = {
-        'type': 'text',
-        'text': message
-    };
+  const target_date = new Date(); //TODO 日付指定はまだ
+  target_date.setDate(1);
+  const end_date = new Date(target_date.getFullYear(), target_date.getMonth(), 1);
+  end_date.setMonth(end_date.getMonth() + 1);
+  end_date.setDate(end_date.getDate() - 1);
 
-    try {
-        await client.replyMessage(messageData.replyToken, postData);
-    } catch(error) {
-        console.log(error);
-    }
+  const amount_str = await getMonthlyAmount(target_date);
+  const message = amount_str
+    ? `期間: ${toYMDString(target_date)}～${toYMDString(end_date)}\nAWS利用料: ${parseFloatAndFloor(amount_str)}$`
+    : "AWS料金の取得に失敗しました😩";
+  const postData = {
+    type: "text",
+    text: message,
+  };
+
+  try {
+    await client.replyMessage(messageData.replyToken, postData);
+  } catch (error) {
+    console.log(error);
+  }
 };
